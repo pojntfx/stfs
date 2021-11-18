f208c090048d1180b3b551ed9ecfa7a9709876fa
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-11-19 00:56:56 +0100

    feat: Add `stfs-restore` command

--- cmd/stfs-restore/main.go
+++ cmd/stfs-restore/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"archive/tar"
 	"bufio"
 	"flag"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/pojntfx/stfs/pkg/controllers"
 )
@@ -13,6 +17,7 @@ func main() {
 	recordSize := flag.Int("recordSize", 20, "Amount of 512-bit blocks per record")
 	record := flag.Int("record", 0, "Record to seek too")
 	block := flag.Int("block", 0, "Block in record to seek too")
+	headerOnly := flag.Bool("headerOnly", false, "Only read the header")
 
 	flag.Parse()
 
@@ -35,11 +40,14 @@ func main() {
 	}
 	defer f.Close()
 
+	var tr *tar.Reader
 	if fileDescription.Mode().IsRegular() {
 		// Seek to record and block
 		if _, err := f.Seek(int64((*recordSize*controllers.BlockSize**record)+*block*controllers.BlockSize), 0); err != nil {
 			panic(err)
 		}
+
+		tr = tar.NewReader(f)
 	} else {
 		// Seek to record
 		if err := controllers.SeekToRecordOnTape(f, int32(*record)); err != nil {
@@ -49,6 +57,26 @@ func main() {
 		// Seek to block
 		br := bufio.NewReaderSize(f, controllers.BlockSize**recordSize)
 		if _, err := br.Read(make([]byte, *block*controllers.BlockSize)); err != nil {
+			panic(err)
+		}
+
+		tr = tar.NewReader(br)
+	}
+
+	hdr, err := tr.Next()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(hdr)
+
+	if !*headerOnly {
+		dst, err := os.OpenFile(filepath.Base(hdr.Name), os.O_WRONLY|os.O_CREATE, hdr.FileInfo().Mode())
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err := io.Copy(dst, tr); err != nil {
 			panic(err)
 		}
 	}
