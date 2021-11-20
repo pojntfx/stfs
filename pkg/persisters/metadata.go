fe1a30b7ba0b13a0512f9bcb784026710ffb1eb1
Felix Pojtinger <felix@pojtinger.com>
2021-11-21 00:50:11 +0100

    feat: Add generic persister for metadata

---
+++ pkg/persisters/metadata.go
@@ -0,0 +1,79 @@
+package persisters
+
+//go:generate sqlboiler sqlite3 -o ../db/sqlite/models/metadata -c ../../../configs/sqlboiler/metadata.yaml
+//go:generate go-bindata -pkg metadata -o ../db/sqlite/migrations/metadata/migrations.go ../../../db/sqlite/migrations/metadata
+
+import (
+	"archive/tar"
+	"context"
+	"database/sql"
+	"encoding/json"
+
+	"github.com/pojntfx/stfs/pkg/db/sqlite/migrations/metadata"
+	models "github.com/pojntfx/stfs/pkg/db/sqlite/models/metadata"
+	migrate "github.com/rubenv/sql-migrate"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+type MetadataPersister struct {
+	*SQLite
+}
+
+func NewMetadataPersister(dbPath string) *MetadataPersister {
+	return &MetadataPersister{
+		&SQLite{
+			DBPath: dbPath,
+			Migrations: migrate.AssetMigrationSource{
+				Asset:    metadata.Asset,
+				AssetDir: metadata.AssetDir,
+				Dir:      "../../../db/sqlite/migrations/metadata",
+			},
+		},
+	}
+}
+
+func (c *MetadataPersister) UpsertHeader(ctx context.Context, record, block int64, hdr *tar.Header) error {
+	paxHeaders, err := json.Marshal(hdr.PAXRecords)
+	if err != nil {
+		return err
+	}
+
+	dbhdr := models.Header{
+		Record:     record,
+		Block:      block,
+		Typeflag:   int64(hdr.Typeflag),
+		Name:       hdr.Name,
+		Linkname:   hdr.Linkname,
+		Size:       hdr.Size,
+		Mode:       hdr.Mode,
+		UID:        int64(hdr.Uid),
+		Gid:        int64(hdr.Gid),
+		Uname:      hdr.Uname,
+		Gname:      hdr.Gname,
+		Modtime:    hdr.ModTime,
+		Accesstime: hdr.AccessTime,
+		Changetime: hdr.ChangeTime,
+		Devmajor:   hdr.Devmajor,
+		Devminor:   hdr.Devminor,
+		Paxrecords: string(paxHeaders),
+		Format:     int64(hdr.Format),
+	}
+
+	if _, err := models.FindHeader(ctx, c.db, dbhdr.Name, models.HeaderColumns.Name); err != nil {
+		if err == sql.ErrNoRows {
+			if err := dbhdr.Insert(ctx, c.db, boil.Infer()); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		return err
+	}
+
+	if _, err := dbhdr.Update(ctx, c.db, boil.Infer()); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/persisters/sqlite.go
@@ -0,0 +1,44 @@
+package persisters
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+	migrate "github.com/rubenv/sql-migrate"
+)
+
+type SQLite struct {
+	DBPath     string
+	Migrations migrate.MigrationSource
+
+	db *sql.DB
+}
+
+func (d *SQLite) Open() error {
+	// Create leading directories for database
+	leadingDir, _ := filepath.Split(d.DBPath)
+	if err := os.MkdirAll(leadingDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	// Open the DB
+	db, err := sql.Open("sqlite3", d.DBPath)
+	if err != nil {
+		return err
+	}
+
+	// Configure the db
+	db.SetMaxOpenConns(1) // Prevent "database locked" errors
+	d.db = db
+
+	// Run migrations if set
+	if d.Migrations != nil {
+		if _, err := migrate.Exec(d.db, "sqlite3", d.Migrations, migrate.Up); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
