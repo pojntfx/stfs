4d29621a6a58084c78d7b4849fa43c498ce10e3e
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-12-17 18:07:07 +0100

    feat: Add `stat` cmd

---
+++ cmd/stbak/cmd/inventory_stat.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"github.com/pojntfx/stfs/internal/logging"
+	"github.com/pojntfx/stfs/internal/persisters"
+	"github.com/pojntfx/stfs/pkg/config"
+	"github.com/pojntfx/stfs/pkg/inventory"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var inventoryStatCmd = &cobra.Command{
+	Use:     "stat",
+	Aliases: []string{"sta", "s"},
+	Short:   "Get information on a file or directory on tape or tar file",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := viper.BindPFlags(cmd.PersistentFlags()); err != nil {
+			return err
+		}
+
+		metadataPersister := persisters.NewMetadataPersister(viper.GetString(metadataFlag))
+		if err := metadataPersister.Open(); err != nil {
+			return err
+		}
+
+		if _, err := inventory.Stat(
+			config.MetadataConfig{
+				Metadata: metadataPersister,
+			},
+
+			viper.GetString(nameFlag),
+
+			logging.NewLogger().PrintHeader,
+		); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	inventoryStatCmd.PersistentFlags().StringP(nameFlag, "n", "", "File or directory to get info for")
+
+	viper.AutomaticEnv()
+
+	inventoryCmd.AddCommand(inventoryStatCmd)
+}
