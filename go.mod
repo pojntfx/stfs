ee575c2abc3065f1d1a02c973ba9449140c2bdfb
Felix Pojtinger <felix@pojtinger.com>
2021-12-19 01:12:57 +0100

    feat: Start implementation of testing FTP server

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/andybalholm/brotli v1.0.4
 	github.com/cosnicolaou/pbzip2 v1.0.1
 	github.com/dsnet/compress v0.0.1
+	github.com/fclairamb/ftpserverlib v0.16.0
+	github.com/fclairamb/go-log v0.1.0
 	github.com/friendsofgo/errors v0.9.2
 	github.com/klauspost/compress v1.13.6
 	github.com/klauspost/pgzip v1.2.5
@@ -17,13 +19,13 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.11
 	github.com/pkg/errors v0.9.1
 	github.com/rubenv/sql-migrate v0.0.0-20211023115951-9f02b1e13857
+	github.com/spf13/afero v1.6.0
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.9.0
 	github.com/volatiletech/null/v8 v8.1.2
 	github.com/volatiletech/randomize v0.0.1
 	github.com/volatiletech/sqlboiler/v4 v4.8.3
 	github.com/volatiletech/strmangle v0.0.1
-	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1
 )
 
 require (
@@ -32,12 +34,10 @@ require (
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
-	github.com/sirupsen/logrus v1.4.2 // indirect
-	github.com/spf13/afero v1.6.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
@@ -45,6 +45,7 @@ require (
 	github.com/volatiletech/inflect v0.0.1 // indirect
 	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/gorp.v1 v1.7.2 // indirect
--- internal/fs/filesystem.go
+++ internal/fs/filesystem.go
@@ -43,25 +43,25 @@ func NewFileSystem(
 	}
 }
 
-func (S *FileSystem) Name() string {
+func (f *FileSystem) Name() string {
 	log.Println("FileSystem.Name")
 
 	return "STFS"
 }
 
-func (S *FileSystem) Create(name string) (afero.File, error) {
+func (f *FileSystem) Create(name string) (afero.File, error) {
 	log.Println("FileSystem.Name", name)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) Mkdir(name string, perm os.FileMode) error {
+func (f *FileSystem) Mkdir(name string, perm os.FileMode) error {
 	log.Println("FileSystem.Mkdir", name, perm)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) MkdirAll(path string, perm os.FileMode) error {
+func (f *FileSystem) MkdirAll(path string, perm os.FileMode) error {
 	log.Println("FileSystem.MkdirAll", path, perm)
 
 	panic(ErrNotImplemented)
@@ -99,67 +99,89 @@ func (f *FileSystem) Open(name string) (afero.File, error) {
 	), nil
 }
 
-func (S *FileSystem) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
+func (f *FileSystem) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
 	log.Println("FileSystem.OpenFile", name, flag, perm)
 
-	panic(ErrNotImplemented)
+	if flag != 0 {
+		// TODO: Implement update and write
+		panic(ErrNotImplemented)
+	}
+
+	// TODO: Implement `perm` support
+
+	return f.Open(name)
 }
 
-func (S *FileSystem) Remove(name string) error {
+func (f *FileSystem) Remove(name string) error {
 	log.Println("FileSystem.Remove", name)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) RemoveAll(path string) error {
+func (f *FileSystem) RemoveAll(path string) error {
 	log.Println("FileSystem.RemoveAll", path)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) Rename(oldname, newname string) error {
+func (f *FileSystem) Rename(oldname, newname string) error {
 	log.Println("FileSystem.Rename", oldname, newname)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) Stat(name string) (os.FileInfo, error) {
+func (f *FileSystem) Stat(name string) (os.FileInfo, error) {
 	log.Println("FileSystem.Stat", name)
 
-	panic(ErrNotImplemented)
+	hdr, err := inventory.Stat(
+		f.metadata,
+
+		name,
+
+		f.onHeader,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, os.ErrNotExist
+		}
+
+		panic(err)
+	}
+
+	return NewFileInfo(hdr), nil
 }
 
-func (S *FileSystem) Chmod(name string, mode os.FileMode) error {
+func (f *FileSystem) Chmod(name string, mode os.FileMode) error {
 	log.Println("FileSystem.Chmod", name, mode)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) Chown(name string, uid, gid int) error {
+func (f *FileSystem) Chown(name string, uid, gid int) error {
 	log.Println("FileSystem.Chown", name, uid, gid)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) Chtimes(name string, atime time.Time, mtime time.Time) error {
+func (f *FileSystem) Chtimes(name string, atime time.Time, mtime time.Time) error {
 	log.Println("FileSystem.Chtimes", name, atime, mtime)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) LstatIfPossible(name string) (os.FileInfo, bool, error) {
+func (f *FileSystem) LstatIfPossible(name string) (os.FileInfo, bool, error) {
 	log.Println("FileSystem.LstatIfPossible", name)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) SymlinkIfPossible(oldname, newname string) error {
+func (f *FileSystem) SymlinkIfPossible(oldname, newname string) error {
 	log.Println("FileSystem.SymlinkIfPossible", oldname, newname)
 
 	panic(ErrNotImplemented)
 }
 
-func (S *FileSystem) ReadlinkIfPossible(name string) (string, error) {
+func (f *FileSystem) ReadlinkIfPossible(name string) (string, error) {
 	log.Println("FileSystem.ReadlinkIfPossible", name)
 
 	panic(ErrNotImplemented)
